e52c61fde35a40994504ffddd795ed02cb4ed1a6
Hurma <hurma@local.host>
2023-07-05 15:55:38 +0300

    05.07.2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './components/Header/Header';
+import Sort from './components/Sort/Sort';
+import Categories from './components/Categories/Categories';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+
+import './scss/app.scss';
+
+const items = [
+  { title: 'Мексиканская', price: '2234' },
+  { title: 'Веганская', price: '5643' },
+  { title: 'Угандская', price: '9566' },
+  { title: 'Итальянская', price: '1234' },
+  { title: '4 сыра', price: '2345' },
+  { title: 'Неополитана', price: '5632' },
+  { title: 'С салом', price: '6674' },
+  { title: 'С брынзой', price: '7375' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            {items.map(({ title, price }) => (
+              <PizzaBlock title={title} price={price} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
