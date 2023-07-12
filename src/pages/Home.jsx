849986b549ed691bbb6777c31077478fb5f0b29c
Hurma <hurma@local.host>
2023-07-12 19:21:23 +0300

    cange app name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
   React.useEffect(() => {
     setIsLoading(true);
-    const url = new URL('http://127.0.0.1:5000/api/pizzas/');
+    const url = new URL('http://127.0.0.1:5000/api/pizzas');
     const params = new URLSearchParams();
     params.append('category', categoryId);
     params.append('sorttype', sortType);
